34fcb1523a11875ad313bff4a244eb8b96004556
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-02-23 14:43:33 +0600

    Adds rector php

---
+++ rector.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+    ]);
+
+    // get services (needed for register a single rule)
+    $services = $containerConfigurator->services();
+
+    $paths = glob(__DIR__ . '/vendor/rector/rector/rules/DowngradePhp7*/Rector/*/*.php');
+    $paths = array_merge($paths, glob(__DIR__ . '/vendor/rector/rector/rules/DowngradePhp56/Rector/*/*.php'));
+
+    $serviceClasses = [];
+
+    foreach ($paths as $path) {
+        $tokens = token_get_all(file_get_contents($path));
+
+        foreach ($tokens as $token) {
+            if ($token[0] === 265) {
+                $serviceClasses[] = $token[1] . '\\' . basename($path, '.php');
+                break;
+            }
+        }
+    }
+
+    // register rules
+    foreach (array_unique($serviceClasses) as $serviceClass) {
+        $services->set($serviceClass);
+    }
+};
